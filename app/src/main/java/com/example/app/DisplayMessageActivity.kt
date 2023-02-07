fe24b0897cb42d1f4642d7ac1a20b6a38e1ee78c
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-02-07 15:26:13 +0100

    Added second activity

---
+++ app/src/main/java/com/example/app/DisplayMessageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+    }
+}
